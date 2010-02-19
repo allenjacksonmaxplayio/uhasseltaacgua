d3cc1322b880a16a909673689767396d9e6696f5
nick.defrangh <nick.defrangh@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-02-19 20:44:43 +0000

    Added GUI Webcam demo, be sure to download "Win-Package - GUI - 1.1.zip" first and set the Working directory!

--- trunk/HovercraftUniverse/GUI/Test.cpp
+++ trunk/HovercraftUniverse/GUI/Test.cpp
@@ -1,4 +1,16 @@
+#include "HikariDemo.h"
+#include "GUIManager.h"
+#include <windows.h>
 
 int main(int argc, char *argv[]) {
-	return 1;
+	try {
+		HikariDemo demo;
+		while(!demo.shouldQuit) {
+			demo.update();
+		}
+	} catch(Ogre::Exception& e) {
+		ShowCursor(true);
+	}
+
+	return 0;
 }
