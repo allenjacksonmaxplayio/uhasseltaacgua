5276288b428d43767092871441e6fcbbf189a125
nick.defrangh <nick.defrangh@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-02-21 22:04:56 +0000

    Small change...

--- trunk/HovercraftUniverse/HovercraftUniverse/HUD.cpp
+++ trunk/HovercraftUniverse/HovercraftUniverse/HUD.cpp
@@ -7,8 +7,7 @@ namespace HovUni {
 HUD::HUD() {
 	//Initialise all the object, this list should be controllable by scripts/Configuration files
 	mSpeedometer = new Speedometer("speedometer", "speedometer.swf", 360, 360, Hikari::BottomRight);
-	//TODO: Should be accesible trough the overlay object itself!
-	//mSpeedometer->getFlashControl()->setTransparent(true, true);
+	mSpeedometer->setBParameter(BasicOverlay::OverlayParams_B::ALPHAHACK, true);
 
 	this->addOverlay("speedometer", mSpeedometer);
 }
