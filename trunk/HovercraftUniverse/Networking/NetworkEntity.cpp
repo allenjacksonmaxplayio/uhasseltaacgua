91d0f833593b5af2cc50c0193715be963df58e4e
berghmans.olivier <berghmans.olivier@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-02-22 14:37:48 +0000

    Networking in HovercraftUniverse

---
+++ trunk/HovercraftUniverse/Networking/NetworkEntity.cpp
@@ -0,0 +1,37 @@
+#include "NetworkEntity.h"
+
+namespace HovUni {
+
+NetworkEntity::NetworkEntity(void) : mNode(0) {
+	mNode = new ZCom_Node();
+}
+
+NetworkEntity::~NetworkEntity(void) {
+	if (mNode) {
+		delete mNode;
+		mNode = 0;
+	}
+}
+
+void NetworkEntity::processEvents() {
+	while (mNode->checkEventWaiting()) {
+		eZCom_Event type;
+		eZCom_NodeRole remote_role;
+		ZCom_ConnID conn_id;
+
+		ZCom_BitStream* data = mNode->getNextEvent(&type, &remote_role, &conn_id);
+
+		if (remote_role == eZCom_RoleAuthority && type == eZCom_EventRemoved) {
+			mDeleteMe = true;
+		} else if (type == eZCom_EventUser) {
+			// Delegate to the user events callback
+			parseEntityEvents(data);
+		}
+	}
+}
+
+ZCom_Node* NetworkEntity::getNetworkNode() {
+	return mNode;
+}
+
+}
