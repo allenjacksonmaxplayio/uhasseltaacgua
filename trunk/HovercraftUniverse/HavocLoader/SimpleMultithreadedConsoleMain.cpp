1307ed0869984fd60c9946d42a42fb66f35e1273
pintens.pieter.jan@gmail.com <pintens.pieter.jan@gmail.com@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-02-25 20:00:15 +0000

    *it works: InputManager in the HavocLoader captures keys

--- trunk/HovercraftUniverse/HavocLoader/SimpleMultithreadedConsoleMain.cpp
+++ trunk/HovercraftUniverse/HavocLoader/SimpleMultithreadedConsoleMain.cpp
@@ -1,16 +1,119 @@
+#include "windows.h"
+
 #include "HavocWorldUpdater.h"
-
-#include <OgreRoot.h>
+#include <OgreWindowEventUtilities.h>
 #include "../HovercraftUniverse/InputManager.h"
-using HovUni::InputManager;
+#include <OgreRoot.h>
+using namespace HovUni;
 
-int main( int argc, char * argv [] )
-{
-		//we need ogre for IO
-		Ogre::Root * mOgreRoot = new Ogre::Root();
-		mOgreRoot->restoreConfig();
-		Ogre::RenderWindow * window = mOgreRoot->initialise(true, "Hovercraft Universe");
-		InputManager::getSingletonPtr()->initialise(window);
+
+class KeyLister : public OIS::KeyListener {
+public:
+
+	/**
+	 * Notifies all the listeners that the given key was pressed.
+	 *
+	 * @param e the key event that will be propagated to all listeners
+	 * @return whether succesful
+	 */
+	bool keyPressed(const OIS::KeyEvent &e){
+		int text = e.text;
+		return true;
+	}
+
+	/**
+	 * Notifies all the listeners that the given key was released.
+	 *
+	 * @param e the key event that will be propagated to all listeners
+	 * @return whether succesful
+	 */
+	bool keyReleased(const OIS::KeyEvent &e){
+		return true;
+	}
+};
+
+class Application {
+protected:
+
+	/** The root Ogre object */
+	Ogre::Root * mOgreRoot;
+
+	/** The input manager */
+	InputManager * mInputManager;
+
+public:
+
+	/**
+	 * Constructor.
+	 *
+	 */
+	Application(void){}
+
+	/**
+	 * Destructor.
+	 */
+	virtual ~Application(void){}
+
+	/**
+	 * The main method that triggers the application to run.
+	 */
+	void go(){
+	createRoot();
+	setupRenderSystem();
+	createRenderWindow();
+	initializeResourceGroups();
+	setupInputSystem();
+	setupScene();
+	startRenderLoop();
+}
+
+	void createRoot(){
+		mOgreRoot = new Ogre::Root();
+	}
+
+	void setupRenderSystem(){
+	if (!mOgreRoot->restoreConfig() && !mOgreRoot->showConfigDialog()) {
+		// TODO Throw exception
+	}
+	}
+
+	void createRenderWindow(){
+		mOgreRoot->initialise(true, "Hovercraft Universe");
+	}
+
+	void initializeResourceGroups(){
+		Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
+		Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+	}
+
+	void setupScene(){
+		Ogre::SceneManager * sceneMgr = mOgreRoot->createSceneManager(Ogre::ST_GENERIC, "Default");
+		// TODO Create a Camera manager that creates cameras for each gameview, then add this on the fly
+		//		to the viewports
+		Ogre::Camera * cam = sceneMgr->createCamera("Camera");
+		cam->setNearClipDistance(5);
+		Ogre::Viewport * vp = mOgreRoot->getAutoCreatedWindow()->addViewport(cam);
+
+		// TODO Put somewhere else
+		Ogre::SceneNode * node = sceneMgr->getRootSceneNode()->createChildSceneNode("CamNode", Ogre::Vector3(-400, 200, 400));
+		node->yaw(Ogre::Degree(-45));
+		node = node->createChildSceneNode("PitchNode");
+		node->attachObject(cam);
+
+		// TODO Do the operations below belong here or as in separated methods
+		// Set ambient light
+		sceneMgr->setAmbientLight(Ogre::ColourValue(0.25, 0.25, 0.25));
+	}
+
+	void setupInputSystem(){
+		mInputManager = InputManager::getSingletonPtr();
+		mInputManager->initialise(mOgreRoot->getAutoCreatedWindow());
+	}
+	
+	void startRenderLoop() {
+		KeyLister listener;
+
+		HovUni::InputManager::getSingletonPtr()->addKeyListener(&listener,"KB");
 
 		//do havoc sim in background
 		HavocWorldUpdater mHavoc (".\\..\\..\\..\\art\\models\\planetgravity_L4101.hkx");
@@ -25,12 +128,32 @@ int main( int argc, char * argv [] )
 
 		while ( true )
 		{
+			Ogre::WindowEventUtilities::messagePump();
+
+			HovUni::InputManager::getSingletonPtr()->capture();
+
+			mOgreRoot->renderOneFrame(timestep);
+
 			mHavoc.update(timestep);
 
 			// Pause until the actual time has passed
 			while (stopWatch.getElapsedSeconds() < lastTime + timestep);
-			lastTime += timestep;
+			lastTime += timestep;			
 		}
+	}
 
-    return 0;
+};
+
+
+int main( int argc, char * argv[] ){
+//INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT) {
+	Application app;
+
+	try {
+		app.go();
+	} catch (Ogre::Exception & e) {
+		MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+	}
+
+	return 0;
 }
