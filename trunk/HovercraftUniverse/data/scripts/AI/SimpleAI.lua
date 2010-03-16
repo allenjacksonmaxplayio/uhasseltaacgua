2fbc068980d3a35d94dbe084f0497a6210a728e1
dirk.delahaye <dirk.delahaye@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-03-16 00:37:30 +0000

    Slightly less stupid AI! (Only slightly)

--- trunk/HovercraftUniverse/data/scripts/AI/SimpleAI.lua
+++ trunk/HovercraftUniverse/data/scripts/AI/SimpleAI.lua
@@ -6,14 +6,14 @@ AI_NAME = "Simple AI";
 
 --[[
 	Values and functions received from game:
-	
+
 	/** Start an action (press a button) */
 	void startAction(ControllerActionType action);
-	
+
 	/** Stop an action (release a button) */
 	void stopAction(ControllerActionType action);
-	
-	
+
+
 ]]
 -----------------------------
 -- Control enum Constants
@@ -23,6 +23,8 @@ ACCELERATE	=	1;
 BRAKE		=	2;
 TURNLEFT	=	3;
 TURNRIGHT	=	4;
+-----------------------------
+
 
 -----------------------------
 -- STATE Constants
@@ -31,6 +33,16 @@ ST_IDLE 	=	0;
 ST_MOVING 	=	1;
 -----------------------------
 mCurrentState = 0;
+mPosition = 0
+mInitialPosition = 0
+--HovercraftAIController game
+
+
+--Current button state
+mACCELERATE	=	false;
+mBRAKE		=	false;
+mTURNLEFT	=	false;
+mTURNRIGHT	=	false;
 
 function registerController(controllerObj)
 	game = controllerObj;
@@ -40,13 +52,56 @@ function println(msg)
 	game:luaLog(AI_NAME .. " :: " .. msg);
 end
 
+function setPosition(pos)
+	if (mPosition == 0) then
+		mInitialPosition = pos;
+	end
+	mPosition = pos;
+end
 
+threshold = 300;
+
+--[[
+--	Main function
+--	All very hardcoded and just for testing purposes!
+--]]
 function decide()
+	println("Position: " .. mPosition.x .. ", " .. mPosition.y .. ", " .. mPosition.z);
 	--println("AI Deciding"); --too much output :p
 	if (mCurrentState == ST_IDLE) then
 		game:startAction(ACCELERATE);
-		game:startAction(TURNLEFT);
+		mACCELERATE = true;
 		mCurrentState = ST_MOVING;
+	elseif (mCurrentState == ST_MOVING) then
+		distance = mPosition:distance(mInitialPosition);
+		println("Current distance from starting point = " .. distance);
+		if (distance > threshold) then
+			if (mACCELERATE) then
+				--stop
+				game:stopAction(ACCELERATE);
+				mACCELERATE = false;
+				game:stopAction(TURNLEFT);
+				mTURNLEFT = false;
+				--start
+				game:startAction(BRAKE);
+				mBRAKE = true;
+				game:startAction(TURNRIGHT);
+				mTURNRIGHT = true;
+			elseif (mBRAKE) then
+				--stop
+				game:stopAction(BRAKE);
+				mBRAKE = false;
+				game:stopAction(TURNRIGHT);
+				mTURNRIGHT = false;
+				--start
+				game:startAction(ACCELERATE);
+				mACCELERATE = true;
+				game:startAction(TURNLEFT);
+				mTURNLEFT = true;
+			end
+		end
 	end
+
+
 	return 0;
 end
