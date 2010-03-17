496059480d52d3b3a1f41fca11a00f10a5e4f30a
pintens.pieterjan <pintens.pieterjan@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-03-17 16:47:16 +0000

    ** restored DummyHovercraft

--- trunk/HovercraftUniverse/HovercraftUniverse/CheckPointRepresentation.cpp
+++ trunk/HovercraftUniverse/HovercraftUniverse/CheckPointRepresentation.cpp
@@ -2,7 +2,7 @@
 
 namespace HovUni {
 
-CheckPointRepresentation::CheckPointRepresentation(CheckPoint * entity, Ogre::String mesh, Ogre::SceneManager * sceneMgr) : EntityRepresentation(entity, "EMPTY.mesh",   sceneMgr) {
+CheckPointRepresentation::CheckPointRepresentation(CheckPoint * entity, Ogre::String mesh, Ogre::SceneManager * sceneMgr) : EntityRepresentation(entity, "CheckPoint0Geom.mesh",   sceneMgr) {
 	// Empty
 }
 
