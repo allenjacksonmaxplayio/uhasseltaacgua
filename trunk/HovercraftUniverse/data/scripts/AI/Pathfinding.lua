2b8d4ffe42c18aa8a8a9177aeee4688db0d75de7
dirk.delahaye <dirk.delahaye@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-03-24 21:39:10 +0000

    + Semi-hardcoded path + Improvements to Pathfinding AI

--- trunk/HovercraftUniverse/data/scripts/AI/Pathfinding.lua
+++ trunk/HovercraftUniverse/data/scripts/AI/Pathfinding.lua
@@ -1,8 +1,10 @@
 --[[
 --	Pathfinding AI
 --	@author Dirk Delahaye, 23/03/2010
+--
+-- TODO (24/03/2010): Implement "arrival" behaviour.
 --]]
-AI_NAME = "Pathfinding AI";
+AI_NAME = "PathAI"; --Not too long, this appears in every logline
 dofile("scripts/AI/Utils.lua");
 
 -----------------------------
@@ -34,7 +36,7 @@ mInitialPosition = 0
 -- Pathfinding Constants
 -----------------------------
 PATH_PROBELENGTH = 50;
-PATH_RADIUS = 80;
+PATH_RADIUS = 2.5;
 -----------------------------
 
 
@@ -56,7 +58,14 @@ function setEntity(entity)
 	mEntity = entity;
 end
 
-epsilon = 0.01; --epsilon for avoiding oscillations
+--[[
+-- Sets the path.
+-- @param path A table of Entity objects.
+-- @return void
+--]]
+function setPath(path)
+	mPath = path;
+end
 
 function toString(v)
 	return "(" .. v.x .. ", " .. v.y .. ", " .. v.z .. ")";
@@ -80,15 +89,15 @@ function decide()
 	--############################### PATH FINDING
 	local probe = position + (velocity * PATH_PROBELENGTH);
 	println("Probe: " .. toString(probe));
-	-- TODO hardcoded path
-	local path = {Vector3(-50, 0, 100), Vector3(0, 0, 50), Vector3(50, 0, 0)};
 	--LUA TABLES START AT INDEX 1
 	local distanceToPath = math.huge; --math.huge = +infinity
 	local pathIndex = 0;
-	for key,value in pairs(path) do
+	for key,value in pairs(mPath) do
 		if (key ~= 1) then
-			local p0 = path[key-1];
-			local p1 = path[key];
+			--they are Entities.
+			local p0 = mPath[key-1]:getPosition();
+			local p1 = mPath[key]:getPosition();
+			
 			local project = project(probe, p0, p1);
 			if (position:distance(project) < distanceToPath) then
 				distanceToPath = position:distance(project);
@@ -97,40 +106,44 @@ function decide()
 		end
 	end
 
-	local p0 = path[pathIndex-1];
-	local p1 = path[pathIndex];
+	local p0 = mPath[pathIndex-1]:getPosition();
+	local p1 = mPath[pathIndex]:getPosition();
 	local project = project(probe, p0, p1);
 	distanceToPath = position:distance(project);
-	println("Closest path lies between " .. toString(p0) .. " and " .. toString(p1) .. ",projected point ".. toString(project) .." at " .. distanceToPath .. " units distance from probe.");
+	println("Closest pathline lies between " .. toString(p0) .. " and " .. toString(p1));
+	println("projected point ".. toString(project) .." at " .. distanceToPath .. " units distance from probe.");
 	local targetPosition;
+	local distanceThreshold = 2;
 	if (distanceToPath > PATH_RADIUS) then
 		--Seek the path
 		println("Seeking " .. toString(project));
 		targetPosition = project;
 	else
-		--No corrective steering required. Force this by setting target to the predicted position.
-		println("No corrective steering required. Seeking " .. toString(probe));
-		targetPosition = probe;
+		--No corrective steering required.
+		println("No corrective steering required.");
+		game:setAction(TURNRIGHT, false);
+		game:setAction(TURNLEFT, false);
+		if (position:distance(p1) > distanceThreshold) then
+			game:setAction(ACCELERATE, true);
+		else
+			game:setAction(ACCELERATE, false);
+		end
+		return 0;
 	end
 
 	--################################## END PATH FINDING
 
-
-	--println("");
-	
-	println("Target: " .. toString(targetPosition));
-	--Calculate orientation
+	--println("Target: " .. toString(targetPosition));
 	targetOrientation = targetPosition - position;
 	targetOrientation:normalise();
-	--println("targetOrientation: " .. targetOrientation.x .. ", " .. targetOrientation.y .. ", " .. targetOrientation.z);
 	local myOrientation = mEntity:getOrientation();
-	--println("myOrientation: " .. myOrientation.x .. ", " .. myOrientation.y .. ", " .. myOrientation.z);
 	local side = myOrientation:crossProduct(mEntity:getUpVector());
 	side = side:dotProduct(targetOrientation);
-	--println("side = " .. side);
-
-	--deltaOrientation is the unit vector telling us which way to go, from our current direction.
-	if (position:distance(targetPosition) > 20) then
+	
+	
+	
+	epsilon = 0.0001; --epsilon for avoiding steering oscillations
+	if (position:distance(targetPosition) > distanceThreshold) then
 		game:setAction(ACCELERATE, true);
 		if (side > epsilon) then
 			println("Turning Right");
@@ -147,6 +160,8 @@ function decide()
 		end
 	else
 		game:setAction(ACCELERATE, false);
+		game:setAction(TURNRIGHT, false);
+		game:setAction(TURNLEFT, false);
 	end
 
 	return 0;
