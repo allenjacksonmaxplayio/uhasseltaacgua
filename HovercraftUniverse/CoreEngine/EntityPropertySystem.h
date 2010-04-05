cba937185125cf358849bec0ca7127e59de5d3d2
pintens.pieter.jan <pintens.pieter.jan@gmail.com>
2010-04-05 08:38:03 +0000

    ** Property system

--- HovercraftUniverse/CoreEngine/EntityPropertySystem.h
+++ HovercraftUniverse/CoreEngine/EntityPropertySystem.h
@@ -2,13 +2,13 @@
 #define EntityProperty_H_
 
 #include <zoidcom/zoidcom.h>
-#include <OgreString.h>
 #include <map>
 
 namespace HovUni {
 
 class Entity;
 class EntityProperty;
+class EntityPropertyMapReplicator;
 
 /**
  * The container for properties, it is a wrapper arround a map
@@ -19,6 +19,11 @@ class EntityPropertyMap {
 private:
 
 	/**
+	 * The replicator is our friend
+	 */
+	friend class EntityPropertyMapReplicator;
+
+	/**
 	 * The entity the map belongs to
 	 */
 	Entity * mEntity;
@@ -26,16 +31,16 @@ private:
 	/**
 	 * The actual map
 	 */
-	std::map<Ogre::String,EntityProperty*> mMap;
+	std::map<int,EntityProperty*> mMap;
 
 	/**
 	 * The current version
 	 */
-	int mUpdate;
+	signed char mUpdate;
 
 public:
-	typedef std::pair<Ogre::String,EntityProperty*> Property;
-	typedef std::map<Ogre::String,EntityProperty*> PropertyMap;
+	typedef std::pair<int,EntityProperty*> Property;
+	typedef std::map<int,EntityProperty*> PropertyMap;
 
 	/**
 	 * Constructor
@@ -60,14 +65,14 @@ public:
 	 * 
 	 * @return true if the property with given key exists in the map, false otherwise
 	 */
-	bool hasProperty( const Ogre::String& key ) const;
+	bool hasProperty( int key ) const;
 
 	/**
 	 * Get the property with given key
 	 * 
 	 * @return the property with given key, null if not found
 	 */
-	EntityProperty * getProperty( const Ogre::String& key );
+	EntityProperty * getProperty( int key );
 
 	/**
 	 * Remove propery with given key from the map
@@ -76,7 +81,7 @@ public:
 	 * @param del, if true the property will be deleted aswell
 	 * @return true if property removed, false if no such property found
 	 */
-	bool removeProperty ( const Ogre::String& key, bool del = true );
+	bool removeProperty ( int key, bool del = true );
 
 	/**
 	 * Remove propery from the map
@@ -93,7 +98,7 @@ public:
 	 * @param key, the key of the property
 	 * @return the property removed from the map, null if no such property found
 	 */
-	EntityProperty * removeProperty ( const Ogre::String& key );
+	EntityProperty * removeProperty ( int key );
 
 	/**
 	 * Remove propery from the map
@@ -126,7 +131,7 @@ protected:
 	/**
 	 * The type
 	 */
-	Ogre::String mType;	
+	int mType;	
 
 	/**
 	 * The entity the property belongs to
@@ -136,7 +141,7 @@ protected:
 	/**
 	 * The version of the property
 	 */
-	int mVersion;
+	signed char mVersion;
 
 public:
 
@@ -146,7 +151,7 @@ public:
 	 * @param type
 	 * @param entity
 	 */
-	EntityProperty( const Ogre::String& type );
+	EntityProperty( int type );
 
 	/**
 	 * Destructor
@@ -163,42 +168,52 @@ public:
 	 * Get the property type
 	 * @return the property type
 	 */
-	const Ogre::String& getProperyType() const;
+	int getProperyType() const;
 
 	/**
 	 * Write this property to bitstream
-	 * This method must be overwriten and the parent must be called
 	 *
 	 * @param bitstream
 	 */
-	void write(ZCom_BitStream * bitstream);
+	virtual void write(ZCom_BitStream * bitstream) = 0;
 
 	/**
 	 * Read this property to bitstream
-	 * This method must be overwriten and the parent must be called
 	 *
 	 * @param bitstream
 	 */
-	void read(ZCom_BitStream * bitstream);
+	virtual void read(ZCom_BitStream * bitstream) const = 0;
 
 	/**
 	 * Call this method when there is an update that needs to be propagated over network
 	 */
 	void update();
-
 };
 
 /**
  * Replicator for the property map
  */
-class EntityPropertyMapReplicator : public ZCom_ReplicatorBasic {
+class EntityPropertyMapReplicator : public ZCom_ReplicatorAdvanced {
 
 private:
+
+	enum TYPE {
+		ADD = 0x0,
+		UPDATE = 0x1,
+		REMOVE = 0x2
+	};
 
 	/**
 	 * Data
 	 */
 	EntityPropertyMap& mData;
+
+	/**
+	 * The version of the map
+	 */
+	unsigned char mUpdate;
+
+	//keep copy of the authority
 
 public:
 
@@ -206,46 +221,34 @@ public:
 
 	EntityPropertyMapReplicator(EntityPropertyMap& data, zU8 _flags, zU8 _rules, zU8 _intercept_id = 0, zS16 _mindelay = -1, zS16 _maxdelay = -1);
 
-	/**
-	 * Check the state of the variable to replicate
-	 *
-	 * @return true if there needs to be an update
-	 */
-	bool checkState();
+	virtual void onConnectionAdded (ZCom_ConnID _cid, eZCom_NodeRole _remoterole);
+ 	//A node has become relevant on the specified connection and thus to this replicator, too. 
 
-	/**
-	 * Pack the data in the stream in order to send an update
-	 *
-	 * @param _stream the stream
-	 */
-	void packData(ZCom_BitStream *_stream);
+	virtual void onConnectionRemoved (ZCom_ConnID _cid, eZCom_NodeRole _remoterole){
+	}
+ 	//The node on this connection is no longer relevant. 
 
-	/**
-	 * Unpack the data from a stream after receiving an update
-	 *
-	 * @param _stream the stream
-	 * @param _store whether to store the new data
-	 * @param _estimated_time_sent indicator of when the data was approximately sent
-	 */
-	void unpackData(ZCom_BitStream *_stream, bool _store, zU32 _estimated_time_sent);
+	virtual void onDataReceived (ZCom_ConnID _cid, eZCom_NodeRole _remoterole, ZCom_BitStream &_stream, bool _store, zU32 _estimated_time_sent);
+ 	//Data has been received from the replicator of a remote node. 
 
-	/**
-	 * Peek at the data without actually unpacking it
-	 */
-	void* peekData();
+	virtual void onLocalRoleChanged (eZCom_NodeRole _oldrole, eZCom_NodeRole _newrole){
+	}
+ 	//The role of the local node which owns this replicator has changed. 
 
-	/**
-	 * Clear data used when peeking at data
-	 */
-	void clearPeekData();
+	virtual void onPacketReceived (ZCom_ConnID _cid){
+	}
+ 	//The given connection has received a packet. 
+	
+	virtual void onPreSendData (ZCom_ConnID _cid, eZCom_NodeRole _remoterole, zU32 *_lastupdate){
+	}
+ 	//Zoidcom is about to transmit data to the given client. 
+	
+	virtual void onRemoteRoleChanged (ZCom_ConnID _cid, eZCom_NodeRole _oldrole, eZCom_NodeRole _newrole){
+	}
+ 	//The role of a node on a remote connection has changed it's role. 
 
-	/**
-	 * Process
-	 *
-	 * @param _localrole the local node role
-	 * @param _simulation_time_passed the time passed in the simulation
-	 */
-	void Process(eZCom_NodeRole _localrole, zU32 _simulation_time_passed);
+	virtual void Process (eZCom_NodeRole _localrole, zU32 _simulation_time_passed);
+ 	//Do any kind of processing. 
 
 };
 
