6a61ed6f3bab7f1a1721930bb889fb0fe8844bcf
berghmans.olivier <berghmans.olivier@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-04-15 21:01:12 +0000

    RaceState now goes through all states: - After 5 seconds there is a transition from COUNTDOWN to RACING - After 15 seconds there is a transition from RACING to FINISHING - After 3 seconds there is a transition from FINISHING to CLEANUP

---
+++ HovercraftUniverse/Utils/Timing.h
@@ -0,0 +1,43 @@
+#ifndef TIMING_H_
+#define TIMING_H_
+
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+
+namespace HovUni {
+
+/**
+ * This is an helper class that provides some timing functions
+ *
+ * @author Olivier Berghmans
+ */
+class Timing {
+private:
+	/** The start time */
+	boost::posix_time::ptime mStart;
+
+public:
+	/**
+	 * Constructor
+	 */
+	Timing();
+
+	/**
+	 * Destructor
+	 */
+	virtual ~Timing();
+
+	/**
+	 * Get the elapsed time
+	 *
+	 * @return the elapsed time in milliseconds
+	 */
+	long elapsed() const;
+
+	/**
+	 * Restart the timer
+	 */
+	void restart();
+};
+
+}
+#endif //TIMING_H_
