2c7b5b4015a97287c656cc861a3558c09088cc67
pintens.pieterjan <pintens.pieterjan@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-04-28 14:51:39 +0000

    ** Update velocity

--- HovercraftUniverse/HovercraftUniverse/UpdatePositionAction.cpp
+++ HovercraftUniverse/HovercraftUniverse/UpdatePositionAction.cpp
@@ -18,9 +18,12 @@ void UpdatePositionAction::update(){
 	hkpRigidBody* body = dynamic_cast<hkpRigidBody*>(m_entity);
 	const hkVector4& position = body->getPosition();
 	const hkQuaternion& quaternion = body->getRotation();
+	const hkVector4& speed = body->getLinearVelocity();
+
 	
 	mStaticBody->changePosition(Ogre::Vector3(position(0),position(1),position(2)));
 	mStaticBody->changeOrientation( Ogre::Quaternion( quaternion(3), quaternion(0), quaternion(1), quaternion(2)));
+	mStaticBody->changeVelocity(Ogre::Vector3(speed(0),speed(1),speed(2)));
 }
 
 void UpdatePositionAction::applyAction( const hkStepInfo& stepInfo ){
