44e6faef11400b6a8ccc4d862c8dbb8b19e50c39
pintens.pieterjan <pintens.pieterjan@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-05-30 08:39:55 +0000

    ** minor improvement of the sparks position

--- HovercraftUniverse/HovercraftUniverse/HavokHovercraftCollisionEffect.cpp
+++ HovercraftUniverse/HovercraftUniverse/HavokHovercraftCollisionEffect.cpp
@@ -36,31 +36,50 @@ void  HovercraftCollisionListener::contactProcessCallback (hkpContactProcessEven
 		if ( collisiodata->getNumContactPoints () != 0 ){
 			mCollisionEffect->mBounceVector.setZero4();
 
-			Ogre::Vector3 pos(Ogre::Vector3::ZERO);
-			Ogre::Vector3 nor(Ogre::Vector3::ZERO);
+			//Ogre::Vector3 pos(Ogre::Vector3::ZERO);
+			//Ogre::Vector3 nor(Ogre::Vector3::ZERO);
+			hkContactPoint * best = 0;		
 
 			for ( int i = 0; i < collisiodata->getNumContactPoints (); i++ ){
 				hkpProcessCdPoint & point =  collisiodata->getContactPoint (i);
-				mCollisionEffect->mBounceVector.add4(point.m_contact.getNormal());
+				
+				if ( best == 0 ){
+					best = &point.m_contact;
+				}
+				else {
+					if ( fabs(best->getDistance()) > fabs(point.m_contact.getDistance()) ){
+						best = &point.m_contact;
+					}
+				}
 
-				const hkVector4& havpos = point.m_contact.getPosition();
-	
-		
-				pos[0] += havpos(0);
+				//mCollisionEffect->mBounceVector.add4(point.m_contact.getNormal());
+
+				//const hkVector4& havnor =.getNormal();
+				//const hkVector4& havpos = point.m_contact.getPosition();
+
+				//send collision event!
+				//mCollisionEffect->mHovercraft->getEntity()->sendEvent(CollisionEvent(Ogre::Vector3(havpos(0),havpos(1),havpos(2)),Ogre::Vector3(havnor(0),havnor(1),havnor(2))));
+
+
+				/*pos[0] += havpos(0);
 				pos[1] += havpos(1);
-				pos[2] += havpos(2);
+				pos[2] += havpos(2);*/
 			}
 
-			pos /= collisiodata->getNumContactPoints ();
+			/*pos /= collisiodata->getNumContactPoints ();
 			nor[0] = mCollisionEffect->mBounceVector(0);
 			nor[1] = mCollisionEffect->mBounceVector(1);
 			nor[2] = mCollisionEffect->mBounceVector(2);
 			nor.normalise();
-			nor *= -1;
+			nor *= -1;*/
 
 			//send collision event!
-			mCollisionEffect->mHovercraft->getEntity()->sendEvent(CollisionEvent(pos,nor));
+			const hkVector4& havnor = best->getNormal();
+			const hkVector4& havpos = best->getPosition();
+			mCollisionEffect->mHovercraft->getEntity()->sendEvent(CollisionEvent(Ogre::Vector3(havpos(0),havpos(1),havpos(2)),Ogre::Vector3(havnor(0),havnor(1),havnor(2))));
 
+
+			mCollisionEffect->mBounceVector = best->getNormal();
 			mCollisionEffect->mBounceVector.mul4( -1.0f * ((Hovercraft*)mCollisionEffect->mHovercraft->getEntity())->getSpeed()/(hkReal) collisiodata->getNumContactPoints ());
 			mCollisionEffect->mHasCollision= true;
 		}
