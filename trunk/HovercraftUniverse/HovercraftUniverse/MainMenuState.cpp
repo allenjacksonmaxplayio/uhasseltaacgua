387f500cca19071a4a9ec843c334f438d0817811
nick.defrangh <nick.defrangh@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-06-01 11:51:27 +0000

    *hud should not crash anymore

--- trunk/HovercraftUniverse/HovercraftUniverse/MainMenuState.cpp
+++ trunk/HovercraftUniverse/HovercraftUniverse/MainMenuState.cpp
@@ -5,6 +5,7 @@
 #include <tinyxml/tinyxml.h>
 #include <boost/thread/thread_time.hpp>
 #include <HovSound.h>
+#include <NMessageBox.h>
 
 namespace HovUni {
 	MainMenuState::MainMenuState() : mMenu(0), mContinue(true), mLastGUIUpdate(-1), mConnectionThread(0), mConnectionFinished(false) {
@@ -45,7 +46,40 @@ namespace HovUni {
 	}
 
 	void MainMenuState::onCreate() {
-		//Create a new game
+		//NEW CODE THAT STARTS THE SERVER::
+		try {
+			//HovUni::Console::createConsole("HovercraftUniverse Dedicated Server");
+			mLocalServer = new HUDedicatedServer("SingleplayerServer.ini");
+		
+			mLocalServer->init();
+			mLocalServer->run(false);
+
+			//Give the server a second to load, should be enough
+			DWORD dwMilliseconds = 1000;
+			Sleep(dwMilliseconds);
+
+			//Todo prettify catch blocks error msgs like this:
+			//HovUni::MessageBox* msg = new MessageBox("Could not connect to server", "connectionmessage");
+			//GUIManager::getSingletonPtr()->activateOverlay(msg);
+		
+			//HovUni::Console::destroyConsole();
+			onConnect("localhost", this);
+		
+			Ogre::LogManager::getSingletonPtr()->getDefaultLog()->stream() << "[MainMenu]: create game finished";
+
+		} catch (Ogre::Exception& e) {
+			GUIManager::getSingletonPtr()->activateOverlay(new HovUni::NMessageBox(e.getFullDescription(), "OgreException"));
+			//MessageBoxA(NULL, e.getFullDescription().c_str(), "Ogre Exception in starting Single Player Server", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+		} catch (HovUni::Exception& e2) {
+			GUIManager::getSingletonPtr()->activateOverlay(new HovUni::NMessageBox(e2.getMessage(), "HovUniException"));
+			//MessageBoxA(NULL, e2.getMessage().c_str(), "HovUni Exception in starting Single Player Server", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+		} catch (std::exception& e) {
+			GUIManager::getSingletonPtr()->activateOverlay(new HovUni::NMessageBox(e.what(), "stdException"));
+			//MessageBoxA(NULL, e.what(), "Exception in starting Single Player Server", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+		} catch (...) {
+			GUIManager::getSingletonPtr()->activateOverlay(new HovUni::NMessageBox("Unknown fatal Ogre Exception in starting Single Player Server", "unknownException"));
+			//MessageBoxA(NULL, "Unknown fatal Ogre Exception in starting Single Player Server", "Exception in starting Single Player Server", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+		}
 	}
 
 	void MainMenuState::finishConnect() {
