29d7695305613bb0c300e9980a8d0b1d9109201d
nick.defrangh <nick.defrangh@2d55a33c-0a8f-11df-aac0-2d4c26e34a4c>
2010-06-09 12:38:27 +0000

    Simple and Scaling resolution should be finished

--- HovercraftUniverse/GUI/Resolution.h
+++ HovercraftUniverse/GUI/Resolution.h
@@ -25,17 +25,50 @@ namespace HovUni {
 			bool mRelativeHeight;
 
 		public:
-			Resolution(int width, int height, bool relWidth = false, bool relHeight = false) 
-				: mWidth(width), mHeight(height), mRelativeWidth(relWidth), mRelativeHeight(relHeight) {
-			}
+			/**
+			 * Constructor for a fixed resolution. The height and width of this resolution
+			 * are fixed. Don't forget to set the parent resolution if you use relative values!
+			 *
+			 * @param width The width of the resolution
+			 * @param height The height of the resolution
+			 * @param relWidth Boolean to mark the width as relative (default: false)
+			 * @param relHeight Boolean to mark the height as relative (default: false)
+			 */
+			Resolution(int width, int height, bool relWidth = false, bool relHeight = false);
 
+			/**
+			 * Ask if the defined width is relative or absolute
+			 *
+			 * @return True if the width is relative, false otherwise
+			 */
 			virtual inline bool isWidthRelative() { return mRelativeWidth; }
 
-			virtual inline int getWidth() { return mWidth; }
+			/**
+			 * Request the width of this resolution
+			 *
+			 * @return The width of this resolution
+			 * @throws NoParentResolutionException If this resolution is dependent
+			 *		on a parent resolution (relative width) and the parent has not
+			 *		been set.
+			 */
+			virtual inline int getWidth();
 
+			/**
+			 * Ask if the defined height is relative or absolute
+			 *
+			 * @return True if the height is relative, false otherwise
+			 */
 			virtual inline bool isHeightRelative() { return mRelativeHeight; }
 			
-			virtual inline int getHeight() { return mHeight; }
+			/**
+			 * Request the height of this resolution
+			 *
+			 * @return The height of this resolution
+			 * @throws NoParentResolutionException If this resolution is dependent
+			 *		on a parent resolution (relative height) and the parent has not
+			 *		been set.
+			 */
+			virtual inline int getHeight();
 	};
 }
 
--- HovercraftUniverse/GUI/ScalingResolution.cpp
+++ HovercraftUniverse/GUI/ScalingResolution.cpp
@@ -3,12 +3,12 @@
 namespace HovUni {
 
 ScalingResolution::ScalingResolution(IResolution* minResolution, IResolution* minScreenResolution, IResolution* maxResolution, IResolution* maxScreenResolution) 
-	: mMinResolution(minResolution), mMaxResolution(maxResolution), mMinScreenResolution(minScreenResolution), mMaxScreenResolution(maxScreenResolution) {
+	: IResolution(), mMinResolution(minResolution), mMaxResolution(maxResolution), mMinScreenResolution(minScreenResolution), mMaxScreenResolution(maxScreenResolution) {
 
 }
 
 ScalingResolution::ScalingResolution(IResolution* resolution, IResolution* screenResolution) 
-	: mMinResolution(resolution), mMaxResolution(0), mMinScreenResolution(screenResolution), mMaxScreenResolution(0) {
+	: IResolution(), mMinResolution(resolution), mMaxResolution(0), mMinScreenResolution(screenResolution), mMaxScreenResolution(0) {
 
 }
 
@@ -32,6 +32,76 @@ int ScalingResolution::getHeight() {
 	//Calculate the scaled height
 
 	return 1;
+}
+
+int ScalingResolution::calculateDimension(int minSize, int minScreen, bool relSize, int parentSize, int maxSize, int maxScreen) {
+	//Some variables
+	int result;
+	float scale = 0;
+
+	//Calculate the result
+	if ((maxSize == -1) || (maxScreen == -1)) {
+		//We are scaling around a single dimension
+		scale = calculateScale(0, minScreen, parentSize, false);
+
+		float tmpResult = minSize * scale;
+
+		if (relSize) {
+			//tmpResult now contains a percentage, calculate the real value
+			result = (int) ((tmpResult / 100.0f) * parentSize);
+		} else {
+			result = (int) tmpResult;
+		}
+	} else {
+		if (!mPreserverAspectRatio) {
+			if (parentSize <= minScreen) {
+				//We want the minimum size
+				scale = 0.0f;
+			} else if (parentSize >= maxScreen) {
+				//We need to take the maximum size
+				scale = 1.0f;
+			} else {
+				scale = calculateScale(minSize, maxSize, parentSize);
+			}
+		} else {
+			scale = getARScale();
+		}
+
+		//Scale the result
+		float tmpResult = scaleValue((float) minSize, (float) maxSize, scale);
+
+		if (relSize) {
+			//tmpResult now contains a percentage, calculate the real value
+			result = (int) ((tmpResult / 100.0f) * parentSize);
+		} else {
+			result = (int) tmpResult;
+		}
+	}
+
+	return result;
+}
+
+float ScalingResolution::calculateScale(int min, int max, int curr, bool normalize) {
+	float scale = ((float)(curr - min) / (float)(max - min));
+
+	if (normalize) {
+		if (scale < 0.0f) scale = 0.0f;
+		if (scale > 1.0f) scale = 1.0f;
+	}
+
+	return scale;
+}
+
+float ScalingResolution::scaleValue(float minValue, float maxValue, float scale) {
+	return (minValue + scale * (maxValue - minValue));
+}
+
+float ScalingResolution::getARScale() {
+	//Calculate a scale for both the width and height to preserve the aspect ratio
+	float scaleX = calculateScale(mMinResolution->getWidth(), mMaxResolution->getWidth(), mParentResolution->getWidth());
+	float scaleY = calculateScale(mMinResolution->getHeight(), mMaxResolution->getHeight(), mParentResolution->getHeight());
+	//Return the smallest of both scales
+	return (scaleX < scaleY) ? scaleX : scaleY;
 }
 
 }
